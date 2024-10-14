7a6bee5d7d836a4389904cffe23be79565d283d8
Aman Srivastava <amansrivastava57@gmail.com>
2024-10-14 20:47:03 +0530

    Added NativeWind & Router

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Aora</Text>
+    <View  >
+      <Text>Aura!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
